a0eb58dc6714e9c2655f8d120abbcd99a66c85fc
Palanskiy Grigory <g.palanskiy@tg.dunice.net>
2023-02-06 10:06:02 +0300

    Removed spread from reducer

--- src/redux/reducers/newsReducer.js
+++ src/redux/reducers/newsReducer.js
@@ -10,21 +10,18 @@ const newsReducer = (state = initialState, action = null) => {
   switch (action.type) {
     case actionTypes.NEWS_REQUESTED:
       return {
-        ...state,
         news: [],
         isLoading: true,
         error: null,
       };
     case actionTypes.NEWS_RECEIVED:
       return {
-        ...state,
         news: action.payload,
         isLoading: false,
         error: null,
       };
     case actionTypes.NEWS_FAILED:
       return {
-        ...state,
         news: [],
         isLoading: false,
         error: action.error,
