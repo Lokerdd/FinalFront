f97c7ad74796b66b9dd5b106406395c30cb4ee04
Palanskiy Grigory <g.palanskiy@tg.dunice.net>
2023-02-07 16:39:29 +0300

    Front auth validation

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { memo } from 'react';
-import { useFormik } from 'formik';
+import PropTypes from 'prop-types';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 import './AuthForm.css';
 
@@ -8,52 +11,144 @@ const EMAIL = 'email';
 const NAME = 'name';
 const PASSWORD = 'password';
 
-function AuthForm() {
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      name: '',
-      password: '',
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+const schema = (formType) => {
+  const shape = {
+    password: Yup.string()
+      .trim()
+      .min(8, 'At least 8 characters without spaces!')
+      .required('Required'),
+    email: Yup.string()
+      .matches(
+        /^((([0-9A-Za-z]{1}[-0-9A-z.]{1,}[0-9A-Za-z]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u,
+        'Please enter a valid email without russian letters',
+      )
+      .required('Required'),
+  };
+  if (formType === 'Sign Up') {
+    shape.name = Yup.string()
+      .trim()
+      .min(4, 'At least 4 characters without spaces!')
+      .required('Required');
+  }
+  return Yup.object().shape(shape);
+};
+
+function AuthForm({ formType }) {
   return (
-    <form className="auth-form" onSubmit={formik.handleSubmit}>
-      <label htmlFor={EMAIL}>Your email:</label>
-      <input
-        id={EMAIL}
-        name={EMAIL}
-        type={EMAIL}
-        placeholder="Email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
+    <Formik
+      initialValues={{
+        email: '',
+        name: '',
+        password: '',
+      }}
+      validationSchema={schema(formType)}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ errors, touched, handleSubmit }) => (
+        <form
+          className="auth-form"
+          onSubmit={handleSubmit}
+        >
+          <div>
+            <label htmlFor={EMAIL}>Email:</label>
+            <Field
+              id={EMAIL}
+              name={EMAIL}
+              type="text"
+            />
+            {errors.email && touched.email ? (
+              <div className="error">{errors.email}</div>
+            ) : null}
+          </div>
 
-      <label htmlFor={NAME}>Your name:</label>
-      <input
-        id={NAME}
-        name={NAME}
-        type="text"
-        placeholder="Name"
-        onChange={formik.handleChange}
-        value={formik.values.name}
-      />
+          {formType === 'Sign Up'
+            && (
+              <div>
+                <label htmlFor={NAME}>Name:</label>
+                <Field
+                  id={NAME}
+                  name={NAME}
+                  type="text"
+                />
+                {errors.name && touched.name
+                  ? <div className="error">{errors.name}</div>
+                  : null}
+              </div>
+            )}
 
-      <label htmlFor={PASSWORD}>Your password:</label>
-      <input
-        id={PASSWORD}
-        name={PASSWORD}
-        type={PASSWORD}
-        placeholder="Password"
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
+          <div>
+            <label htmlFor={PASSWORD}>Password:</label>
+            <Field
+              id={PASSWORD}
+              name={PASSWORD}
+              type="password"
+            />
+            {errors.password && touched.password ? (
+              <div className="error">{errors.password}</div>
+            ) : null}
+          </div>
 
-      <button type="submit">Submit</button>
-    </form>
+          <button type="submit">{formType}</button>
+        </form>
+      )}
+    </Formik>
   );
 }
+AuthForm.propTypes = {
+  formType: PropTypes.string.isRequired,
+};
+// const formik = useFormik({
+//   initialValues: {
+//     email: '',
+//     name: '',
+//     password: '',
+//   },
+//   onSubmit: (values) => {
+//     alert(JSON.stringify(values, null, 2));
+//   },
+//   validationSchema: { SignupSchema },
+// });
+
+// return (
+//   <form className="auth-form" onSubmit={formik.handleSubmit}>
+//     <label htmlFor={EMAIL}>Your email:</label>
+//     <input
+//       id={EMAIL}
+//       name={EMAIL}
+//       type="text"
+//       placeholder="Email"
+//       onChange={formik.handleChange}
+//       value={formik.values.email}
+//     />
+
+//     {formType === 'Sign Up'
+//       && <label htmlFor={NAME}>Your name:</label>}
+//     {formType === 'Sign Up'
+//       && (
+//         <input
+//           id={NAME}
+//           name={NAME}
+//           type="text"
+//           placeholder="Name"
+//           onChange={formik.handleChange}
+//           value={formik.values.name}
+//         />
+//       )}
+
+//     <label htmlFor={PASSWORD}>Your password:</label>
+//     <input
+//       id={PASSWORD}
+//       name={PASSWORD}
+//       type={PASSWORD}
+//       placeholder="Password"
+//       onChange={formik.handleChange}
+//       value={formik.values.password}
+//     />
+
+//     <button type="submit">{formType}</button>
+//   </form>
+// );
 
 export default memo(AuthForm);
